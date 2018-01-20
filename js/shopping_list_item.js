6c67472c1ebac66003e61efe656074cf032f1db0
Brandon Fowler <bmfowler4@gmail.com>
2018-01-19 15:39:06 -1000

    added tests with functionality for check, uncheck, and render for shopping list item class

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -4,4 +4,16 @@ class ShoppingListItem {
     this.description = description;
     this.is_done = false;
   }
+
+  check() {
+    this.is_done = true;
+  }
+
+  uncheck() {
+    this.is_done = false;
+  }
+
+  render() {
+    return `<li class="completed_${this.is_done}"><span>${this.name}</span> <span>${this.description}</span></li>`;
+  }
 }
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -18,5 +18,40 @@ describe(`ShoppingListItem class setup`, function() {
   it(`should have a property named 'is_done' that is false`, function() {
     expect(new ShoppingListItem(`banana`, `yellow fruit`).is_done).to.be.false;
   });
+});
 
+describe(`ShoppingListItem.check()`, function() {
+  let jacket = new ShoppingListItem(`jacket`, `red jacket`);
+  it(`should be a function`, function() {
+    expect(jacket.check).to.be.a(`function`);
+  });
+
+  it(`should set is_done to true`, function() {
+    jacket.check();
+    expect(jacket.is_done).to.be.true;
+  });
+});
+
+describe(`ShoppingListItem.uncheck()`, function() {
+  let waterBottle = new ShoppingListItem(`water bottle`, `blue water bottle`);
+  it(`should be a function`, function() {
+    expect(waterBottle.uncheck).to.be.a(`function`);
+  });
+
+  it(`should set is_done to false`, function() {
+    waterBottle.uncheck();
+    expect(waterBottle.is_done).to.be.false;
+  });
+});
+
+describe(`ShoppingListItem.render()`, function() {
+  let laptop = new ShoppingListItem(`laptop`, `macbook air 2010`);
+  it(`should be a function`, function() {
+    expect(laptop.render).to.be.a(`function`);
+  });
+
+  it(`should return an html formatted string`, function() {
+    expect(laptop.render()).to.equal(`<li class="completed_${laptop.is_done}"><span>${laptop.name}</span> <span>${laptop.description}</span></li>`);
+    expect(laptop.render()).to.equal(`<li class="completed_false"><span>laptop</span> <span>macbook air 2010</span></li>`);
+  });
 });
