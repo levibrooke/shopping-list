87c63e1c9d0ecf65a1531a8a21c3f56d585bcb2c
Levi Porter <levi@levibrooke.com>
2018-01-19 15:13:36 -1000

    Created test for ShoppingListItem class setup, created passing code

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -1,9 +1,22 @@
-describe('ShoppingListItem class setup', function() {
+const expect = chai.expect;
+const should = chai.should();
 
-  it('should be a class', function() {
-    // expect(ShoppingListItem).to.be
+describe(`ShoppingListItem class setup`, function() {
+
+  it(`should be a class`, function() {
+    expect(new ShoppingListItem()).to.be.instanceof(ShoppingListItem);
   });
 
+  it(`should have a property name 'name' `, function() {
+    expect(new ShoppingListItem(`banana`, `yellow fruit`).name).to.equal(`banana`);
+  });
 
+  it(`should have a property name 'description'`, function() {
+    expect(new ShoppingListItem(`banana`, `yellow fruit`).description).to.equal(`yellow fruit`);
+  });
+
+  it(`should have a property named 'is_done' that is false`, function() {
+    expect(new ShoppingListItem(`banana`, `yellow fruit`).is_done).to.be.false;
+  });
 
 });
